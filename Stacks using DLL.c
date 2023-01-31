36a529d3b23407b4a85dd46496c0f07d79296fdc
rithika2152 <109229925+rithika2152@users.noreply.github.com>
2023-01-31 15:03:48 +0530

    Create Stacks using DLL.c

---
+++ Stacks using DLL.c
@@ -0,0 +1,20 @@
+ #include <stdio.h>
+
+#include <stdlib.h>
+
+struct list
+{
+struct list "prev;
+
+char id[20];
+
+char name[20];
+
+char branch[20];
+
+char area[20];
+
+struct list *next;
+};
+
+struct list *start = NULL;
